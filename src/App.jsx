7b446e0c72fe5c5e5631751774afa28ea85238cc
Fawzia Ruma <fawziaruma0652@gmail.com>
2023-06-09 22:22:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Header from './Component/Header/Header'
+
+function App() {
+  
+
+  return (
+    < div className='App'  >
+
+   <Header/>
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+import  logo from  "../../images/Logo.svg"
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <nav className="header">
+            <img src={logo}></img>
+
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Header
